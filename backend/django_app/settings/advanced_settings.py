1b9f9fd4f9189037fe003a9cc21229230243e373
Aleksandr Sokolov <bigpewm@gmail.com>
2022-11-06 00:57:42 +0300

    fix: database

--- backend/django_app/settings/advanced_settings.py
+++ backend/django_app/settings/advanced_settings.py
@@ -15,6 +15,17 @@ INSTALLED_APPS += [
     'api'
 ]
 
+DATABASES = {
+    'default': {
+        'ENGINE': load_option_from_env('DJANGO_DB_ENGINE', 'django.db.backends.sqlite3', default_is_empty=True),
+        'NAME': load_option_from_env('DB_NAME', BASE_DIR / 'db.sqlite3', default_is_empty=True),
+        'USER': load_option_from_env('DB_USER', 'user', default_is_empty=True),
+        'PASSWORD': load_option_from_env('DB_PASSWORD', 'password', default_is_empty=True),
+        'HOST': load_option_from_env('DJANGO_DB_HOST', 'localhost', default_is_empty=True),
+        'PORT': load_option_from_env('DJANGO_DB_PORT', '5432', default_is_empty=True),
+    }
+}
+
 django_on_heroku.settings(locals())
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
@@ -31,16 +42,6 @@ TIME_ZONE = 'Europe/Moscow'
 SECRET_KEY = load_option_from_env('DJANGO_SECRET_KEY', get_random_secret_key(), default_is_empty=True)
 DEBUG = load_option_from_env('DEBUG', True, transform=numeric_to_bool)
 ALLOWED_HOSTS = load_option_from_env('DJANGO_ALLOWED_HOSTS', '*', transform=split_by_coma)
-DATABASES = {
-    'default': {
-        'ENGINE': load_option_from_env('DJANGO_DB_ENGINE', 'django.db.backends.sqlite3', default_is_empty=True),
-        'NAME': load_option_from_env('DB_NAME', BASE_DIR / 'db.sqlite3', default_is_empty=True),
-        'USER': load_option_from_env('DB_USER', 'user', default_is_empty=True),
-        'PASSWORD': load_option_from_env('DB_PASSWORD', 'password', default_is_empty=True),
-        'HOST': load_option_from_env('DJANGO_DB_HOST', 'localhost', default_is_empty=True),
-        'PORT': load_option_from_env('DJANGO_DB_PORT', '5432', default_is_empty=True),
-    }
-}
 PROJECT_NAME = 'Microbiology'
 PROJECT_DESCRIPTION = 'Microbiology Project'
 API_INFO = {
